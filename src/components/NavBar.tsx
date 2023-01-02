7a79029b37b708e27370c7a9231accb9af540ac8
Alexandre Conte <alexandre.conte10@gmail.com>
2023-01-02 08:08:18 -0300

    NavBar to fixed position

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,15 @@ import styles from '../styles/NavBar.module.css'
 
 export default function NavBar() {
   return (
-    <nav className={styles.navbar}>
-      <ul>
-        <NavBarItem text="Principal" href="/" src="/images/header-icons/home.svg"/>
-        <NavBarItem text="Certificados" href="/certificates" src="/images/header-icons/certificates.svg"/>
-        <NavBarItem text="Projetos" href="/projects" src="/images/header-icons/projects.svg"/>
-      </ul>
-    </nav>
+    <>
+      <div id={styles.navbarSpace}></div>
+      <nav className={styles.navbar}>
+        <ul>
+          <NavBarItem text="Principal" href="/" src="/images/header-icons/home.svg" />
+          <NavBarItem text="Certificados" href="/certificates" src="/images/header-icons/certificates.svg" />
+          <NavBarItem text="Projetos" href="/projects" src="/images/header-icons/projects.svg" />
+        </ul>
+      </nav>
+    </>
   )
 }
